2d0dc25bd579588b6f640c0a4b0937981b294754
HongKyuCho <whghdrb2@hanmail.net>
2017-02-02 10:48:06 +0900

    Initial Commit

---
+++ httpConnectionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  httpConnectionTest
+//
+//  Created by 조홍규 on 2017. 2. 2..
+//  Copyright © 2017년 조홍규. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
